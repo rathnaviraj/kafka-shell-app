9fa9a689cd7eb053732e3d0567c10fd8b0e492b0
Supun Rathnayaka <supun@slatescience.com>
2022-12-26 11:57:20 +0530

    Initial Producer Consumer Docker Images

---
+++ Consumer/consumer.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    while True:
+        print("consumer running....")
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Producer/producer.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    while True:
+        print("Producer running....")
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
