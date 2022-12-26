dfe2342aa91c94b4d83eb3150323292e724941ae
Supun Rathnayaka <supun@slatescience.com>
2022-12-26 14:40:51 +0530

    Kafka Producer

--- Producer/producer.py
+++ Producer/producer.py
@@ -1,8 +1,30 @@
+from kafka import KafkaProducer
+from faker import Faker
+import json
 import time
+import os
+
+BOOTSTRAP_SERVER = os.environ.get('BOOTSTRAP_SERVER')
+KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC')
+
+fake = Faker()
+
+def get_user():
+    return {
+        'name': fake.name(),
+        'address': fake.address()
+    }
+
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
 
 def main():
+    print(f'Server {BOOTSTRAP_SERVER} | Topic {KAFKA_TOPIC}')
+    producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER], value_serializer=json_serializer)
     while True:
-        print("Producer running....")
+        user = get_user()
+        print(user)
+        producer.send(KAFKA_TOPIC, user)
         time.sleep(5)
 
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kafka-shell-app
 Shell application with Kafka and CMAK 
+
+
+docker container run -e BOOTSTRAP_SERVER='kafka-1:9092' -e KAFKA_TOPIC='reg_users' --name prod-1 --rm --network kafka-shell-app_kafka_cluster producer
